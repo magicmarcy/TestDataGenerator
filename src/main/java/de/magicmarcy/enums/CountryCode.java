5751b2431a0cbe17861be92d57f5d0776ae81110
Marcy <marcy@posteo.de>
2025-05-25 01:09:19 +0200

    changed the CountyCodes so only supported languages are here

--- src/main/java/de/magicmarcy/enums/CountryCode.java
+++ src/main/java/de/magicmarcy/enums/CountryCode.java
@@ -6,47 +6,22 @@ package de.magicmarcy.enums;
  * @author magicmarcy
  */
 public enum CountryCode {
-  FRANCE("+33"),
-  USA("+1"),
-  CHINA("+86"),
-  SPAIN("+34"),
-  ITALY("+39"),
-  TURKEY("+90"),
-  UK("+44"),
-  GERMANY("+49"),
-  RUSSIA("+7"),
-  MALAYSIA("+60"),
-  MEXICO("+52"),
-  AUSTRIA("+43"),
-  UKRAINE("+380"),
-  THAILAND("+66"),
-  SAUDIA_ARABIA("+966"),
-  GREECE("+30"),
-  CANADA("+1"),
-  POLAND("+48"),
-  NETHERLANDS("+31"),
-  BELGIUM("+32"),
-  SWITZERLAND("+41"),
-  BRAZIL("+55"),
-  ARGENTINA("+54"),
-  SOUTH_AFRICA("+27"),
-  JAPAN("+81"),
-  SOUTH_KOREA("+82"),
-  AUSTRALIA("+61"),
-  NEW_ZEALAND("+64"),
-  PHILIPPINES("+63"),
-  VIETNAM("+84"),
-  INDONESIA("+62"),
-  SINGAPORE("+65"),
-  HONG_KONG("+852");
+  GERMANY("+49", "de"),
+  USA("+1", "en");
 
   private final String telephoneCode;
+  private final String foldername;
 
-  CountryCode(String telephoneCode) {
+  CountryCode(String telephoneCode, String foldername) {
     this.telephoneCode = telephoneCode;
+    this.foldername = foldername;
   }
 
   public String getTelephoneCode() {
     return telephoneCode;
+  }
+
+  public String getFoldername() {
+    return foldername;
   }
 }
