67859dc88959b9551c938fc2081f9a7fd5d2a6d3
Marcy <marcy@posteo.de>
2025-05-25 15:08:48 +0200

    added the ability to generate a phone number, added tests

---
+++ src/main/java/de/magicmarcy/generator/Telephone.java
@@ -0,0 +1,92 @@
+package de.magicmarcy.generator;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+import de.magicmarcy.enums.CountryCode;
+
+/**
+ * @author magicmarcy
+ */
+public class Telephone {
+
+  public static final int MINUIMUM_PREFIX_NUMBER = 200;
+  public static final int MAXIMUM_PREFOX_NUMBER = 999;
+  public static final int MINIMUM_NUMBER_VALUE = 111111;
+  public static final int MAXIMUM_NUMBER_VALUE = 999999999;
+
+  public static final String SPACE = " ";
+
+  private Telephone() {
+    // private constructor to prevent instantiation
+  }
+
+  public static TelephoneBuilder builder() {
+    return new TelephoneBuilder();
+  }
+
+  public static class TelephoneBuilder {
+    private CountryCode countryCode = null;
+    private int minimumPrefixNumber = MINUIMUM_PREFIX_NUMBER;
+    private int maximumPrefixNumber = MAXIMUM_PREFOX_NUMBER;
+    private int minimumNumberValue = MINIMUM_NUMBER_VALUE;
+    private int maximumNumberValue = MAXIMUM_NUMBER_VALUE;
+
+    public TelephoneBuilder countryCode(CountryCode countryCode) {
+      this.countryCode = countryCode;
+      return this;
+    }
+
+    public TelephoneBuilder minimumPrefixNumber(int minimumPrefixNumber) {
+      this.minimumPrefixNumber = minimumPrefixNumber;
+      return this;
+    }
+
+    public TelephoneBuilder maximumPrefixNumber(int maximumPrefixNumber) {
+      this.maximumPrefixNumber = maximumPrefixNumber;
+      return this;
+    }
+
+    public TelephoneBuilder minimumNumberValue(int minimumNumberValue) {
+      this.minimumNumberValue = minimumNumberValue;
+      return this;
+    }
+
+    public TelephoneBuilder maximumNumberValue(int maximumNumberValue) {
+      this.maximumNumberValue = maximumNumberValue;
+      return this;
+    }
+
+    public String buildOne() {
+      return generateTelephoneNumber();
+    }
+
+    private String generateTelephoneNumber() {
+      final StringBuilder telephone = new StringBuilder();
+
+      // Telephone country prefix
+      if (this.countryCode == null) {
+        telephone.append(pickRandomCountryCode().getTelephoneCode());
+      } else {
+        telephone.append(this.countryCode.getTelephoneCode());
+      }
+
+      telephone.append(SPACE);
+
+      // Telephone prefix
+      telephone.append(Number.builder().range(this.minimumPrefixNumber, this.maximumPrefixNumber).buildOne());
+
+      telephone.append(SPACE);
+
+      // Telephone number
+      telephone.append(Number.builder().range(this.minimumNumberValue, this.maximumNumberValue).buildOne());
+
+      return telephone.toString();
+    }
+
+    private CountryCode pickRandomCountryCode() {
+      final CountryCode[] enumConstants = CountryCode.class.getEnumConstants();
+
+      return enumConstants[ThreadLocalRandom.current().nextInt(enumConstants.length)];
+    }
+  }
+}
---
+++ src/test/java/de/magicmarcy/generator/TelephoneTest.java
@@ -0,0 +1,45 @@
+package de.magicmarcy.generator;
+
+import de.magicmarcy.enums.CountryCode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author magicmarcy
+ */
+public class TelephoneTest {
+
+  @Test
+  void get_a_random_german_telephone_number() {
+    final CountryCode countryCode = CountryCode.GERMANY;
+    final String telephonePrefix = countryCode.getTelephoneCode();
+
+    final String telephone = Telephone.builder().countryCode(countryCode).buildOne();
+    final String[] telephoneParts = telephone.split(Telephone.SPACE);
+
+    Assertions.assertTrue(telephone.startsWith(telephonePrefix));
+    Assertions.assertEquals(countryCode.getTelephoneCode(), telephoneParts[0]);
+    Assertions.assertTrue(Integer.parseInt(telephoneParts[1]) >= Telephone.MINUIMUM_PREFIX_NUMBER);
+    Assertions.assertTrue(Integer.parseInt(telephoneParts[1]) <= Telephone.MAXIMUM_PREFOX_NUMBER);
+
+    Assertions.assertTrue(Integer.parseInt(telephoneParts[2]) >= Telephone.MINIMUM_NUMBER_VALUE);
+    Assertions.assertTrue(Integer.parseInt(telephoneParts[2]) <= Telephone.MAXIMUM_NUMBER_VALUE);
+  }
+
+  @Test
+  void get_a_random_english_telephone_number() {
+    final CountryCode countryCode = CountryCode.USA;
+    final String telephonePrefix = countryCode.getTelephoneCode();
+
+    final String telephone = Telephone.builder().countryCode(countryCode).buildOne();
+    final String[] telephoneParts = telephone.split(Telephone.SPACE);
+
+    Assertions.assertTrue(telephone.startsWith(telephonePrefix));
+    Assertions.assertEquals(countryCode.getTelephoneCode(), telephoneParts[0]);
+    Assertions.assertTrue(Integer.parseInt(telephoneParts[1]) >= Telephone.MINUIMUM_PREFIX_NUMBER);
+    Assertions.assertTrue(Integer.parseInt(telephoneParts[1]) <= Telephone.MAXIMUM_PREFOX_NUMBER);
+
+    Assertions.assertTrue(Integer.parseInt(telephoneParts[2]) >= Telephone.MINIMUM_NUMBER_VALUE);
+    Assertions.assertTrue(Integer.parseInt(telephoneParts[2]) <= Telephone.MAXIMUM_NUMBER_VALUE);
+  }
+}
